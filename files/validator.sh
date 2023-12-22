fd7bc6dc39eecc177327ac90722b6320a4b78c1e
NetworkBanshee <41436940+NetworkBanshee@users.noreply.github.com>
2023-12-22 10:02:12 -0500

    Update validator.sh

--- files/validator.sh
+++ files/validator.sh
@@ -10,12 +10,15 @@ exec /mnt/solana/target/release/solana-validator \
 --rpc-port 8899 \
 --dynamic-port-range 8002-8099 \
 --no-port-check \
+--only-known-rpc \
 --gossip-port 8001 \
 --no-voting \
 --private-rpc \
 --rpc-bind-address 0.0.0.0 \
 --enable-cpi-and-log-storage \
 --account-index program-id \
+--account-index spl-token-owner \
+--account-index spl-token-mint \
 --enable-rpc-transaction-history \
 --wal-recovery-mode skip_any_corrupted_record \
 --log /mnt/logs/solana-validator.log \
@@ -30,4 +33,8 @@ exec /mnt/solana/target/release/solana-validator \
 --accounts-index-memory-limit-mb 350 \
 --account-index-exclude-key kinXdEcpDQeHPEuQnqmUgtYykqKGVFq6CeVX5iAHJq6 \
 --tpu-use-quic \
+--known-validator GwHH8ciFhR8vejWCqmg8FWZUCNtubPY2esALvy5tBvji \
+--known-validator 4JahMMrVRS1gimWoXpD5H6KwKc2MrsoTDFMaStMttL1E \
+--known-validator 5FbKKGdEaFcxGxxaLKVvBes2JxiKbreh8w2ZpMcSQ2a5 \
+--known-validator 7Hp1e6BrTBkbBN4wFiNmycPVPsjvyUUBL2tGhYEMT6gt \
 --known-validator 7qf8xDhtzb4ctRUyJB6eaUXPn69wnx8A8pQVxYejaTLV \
